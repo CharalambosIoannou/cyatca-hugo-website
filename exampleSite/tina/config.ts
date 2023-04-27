5fcbade323cf0f5e223c7948ae8b32412022d9a9
CharalambosIoannou <cioannou1997@gmail.com>
2023-04-27 17:31:25 +0300

    added tina support

---
+++ exampleSite/tina/config.ts
@@ -0,0 +1,81 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: "45045cf7-9996-456b-8983-1c2fbcc444e5", // Get this from tina.io
+  token: "a13514c937d06c4f086840ed3f97606769d60f73", // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "static",
+  },
+  media: {
+    tina: {
+      mediaRoot: "",
+      publicFolder: "static/images",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "content/english/blog",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "string",
+            name: "author",
+            label: "Author",
+            required: true,
+          },
+          {
+            type: "image",
+            name: "image",
+            label: "Image",
+            required: false,
+          },
+          {
+            type: "image",
+            name: "bg_image",
+            label: "Background image",
+            required: false,
+          },
+          {
+            type: "string",
+            name: "categories",
+            label: "Categories",
+            required: false,
+          },
+          {
+            type: "string",
+            name: "tags",
+            label: "Tags",
+            required: false,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
